029dae9218bc7d94d85a47ea59d20c5a1b57ab4c
priyank kumar <tornado.the.whirl@priyanks-MacBook-Pro.local>
2017-04-01 11:10:00 -0400

    Updating server.js to use Passport and Cookie-Parser and Session

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 var express = require('express');
-var app = express();
 
 // For enabling parsing of request body by node.
 var bodyParser = require('body-parser');
+
+// Configure the server to use cookie based session support
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var passport = require('passport');
+
+
+var app = express();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
